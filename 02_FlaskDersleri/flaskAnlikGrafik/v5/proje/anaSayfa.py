afbe7b5e554c2671ee8f599684ebdc5ce68c21dc
keyiflerolsun <keyiflerolsun@gmail.com>
2020-05-27 06:28:30 +0300

    os.getlogin hatası

--- 02_FlaskDersleri/flaskAnlikGrafik/v5/proje/anaSayfa.py
+++ 02_FlaskDersleri/flaskAnlikGrafik/v5/proje/anaSayfa.py
@@ -1,15 +1,18 @@
 from proje import app
 from flask import render_template
-import os, platform, requests
+import os, pwd, platform, requests
 from proje.islem import jsonVeri, anahtarlar
 
 uname = platform.uname()
+
+try: kullanici = os.getlogin()
+except: kullanici = pwd.getpwuid(os.geteuid())[0]
 
 @app.route('/')
 def anaSayfa():
     return render_template('grafik.html', baslik="İşte Bunu Seviyorum",
         sistem = uname.system,
-        kullanici = os.getlogin(),
+        kullanici = kullanici,
         host = uname.node,
         surum = uname.release,
         versiyon = uname.version,
