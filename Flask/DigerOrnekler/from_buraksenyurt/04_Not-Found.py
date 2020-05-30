b4066caba0ef59dfe867ad3c9d9cf1502a56071f
keyiflerolsun <keyiflerolsun@gmail.com>
2020-05-30 11:35:28 +0300

    düzen

--- Flask/DigerOrnekler/from_buraksenyurt/04_Not-Found.py
+++
@@ -1,5 +0,0 @@
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(
-        jsonify({'HTTP 404 Error': 'Aradığınız içerik mevcut değil. Lütfen isteğinizi kontrol edin.'}), 404
-    )
