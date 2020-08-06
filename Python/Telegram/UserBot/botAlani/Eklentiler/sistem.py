0fa7b3859e423e2cfd80ce77e36738657074633a
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2020-08-06 07:49:42 +0300

    Update sistem.py

--- Python/Telegram/UserBot/botAlani/Eklentiler/sistem.py
+++ Python/Telegram/UserBot/botAlani/Eklentiler/sistem.py
@@ -7,11 +7,13 @@ import requests, os, platform
 @Client.on_message(Filters.command(['sistem'], ['!','.','/']) & Filters.me)
 async def sistem(client, message):
     await asyncio.sleep(0.3)
-    await message.edit("‌‌‎__asyncio.sleep(0.3)__")
-
-    mesaj = f"""__Kullanıcı :__ `{os.getlogin()}@{platform.node()}`
-__IP :__ `{requests.get('http://ip.42.pl/raw').text}`
-__OS :__ `{platform.system()} | {platform.release()}`
-__İşlemci :__ `{platform.processor()}`"""
-
-    await message.edit(mesaj)
+    await message.edit("__asyncio.sleep(0.3)__")
+    
+    try:
+        mesaj = f"""__Kullanıcı :__ `{os.getlogin()}@{platform.node()}`
+    __IP :__ `{requests.get('http://ip.42.pl/raw').text}`
+    __OS :__ `{platform.system()} | {platform.release()}`
+    __İşlemci :__ `{platform.processor()}`"""
+        await message.edit(mesaj)
+    
+    except Exception as hata: await message.edit(f"__başaramadık abi__\n\n\t`{hata}`")
